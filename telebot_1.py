0fb9018601b0f4c14cb90742b0e36bfaf4d4160c
Humberto <131888036+Kryon00@users.noreply.github.com>
2023-04-26 14:59:55 -0300

    Update telebot_1.py

--- telebot_1.py
+++ telebot_1.py
@@ -2,16 +2,14 @@ import telebot
 from translate import Translator
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from time import sleep
+from random import randint
 
-API_TOKEN = "------"
+API_TOKEN = "6142954404:AAH-llyGimAd4-1AznsC3mpEIfahl1kr_kI"
 
 bot = telebot.TeleBot(API_TOKEN)
 
-blacklist = ['branco', 'preto', 'negro', 'pardo', 'branca', 'preta', 'negra', 'parda', 'pardola', 'escravo', 'escrava',
-             'favelada', 'louça', 'favelado', 'favelados', 'faveladas', 'brancos', 'pretos', 'negros', 'pardos',
-             'brancas', 'pardas', 'negras', 'pardolas', 'escravos', 'escravas', 'macaco', 'alforriado', 'bicha',
-             'boiola', 'gay', 'viado', 'pardinhas', 'pardinhos', 'pardinho', 'pardinha', 'neguinho', 'neguinha',
-             'negrinho', 'negrinha', 'neguinhos', 'neguinhas', 'negrinhos', 'negrinhas', 'hitler', 'nazismo', 'nazista']
+lista = open("blacklist.txt", 'r').read()
+blacklist = lista.split(' ')
 
 tradutor = Translator(from_lang='pt', to_lang='en')
 sid = SentimentIntensityAnalyzer()
@@ -22,9 +20,33 @@ def separador(texto):
     return a
 
 
+def roll(faces, dados):
+    resultados = []
+    for c in range(1, dados + 1):
+        resultados.append(randint(1, faces))
+    return resultados
+
+
 @bot.message_handler(commands=['help', 'start', 'salve'])
 def send_welcome(message):
     bot.reply_to(message, "Salve!")
+
+
+# /r 1d10
+@bot.message_handler(commands=['r', 'rolar'])
+def rolar(message):
+    try:
+        texto = str(message.text)[3:].split('d')
+        try:
+            n = int(texto[0])
+        except:
+            n = 1
+        f = int(texto[1])
+        dados = roll(f, n)
+        fraseRoll = f'{[x for x in dados]}, Resultado: {sum(dados)}'
+        bot.reply_to(message, fraseRoll)
+    except:
+        bot.reply_to(message, "Erro")
 
 
 def racismo(message):
